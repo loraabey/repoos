1b57a251ab2299bf6e07023c055032b8cd7f108d
loraabey <jotarodio18@gmail.com>
2025-05-31 20:41:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('MORNING ^-^').split('')
+    const titles = ('NIGHT CAA ^-^').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
